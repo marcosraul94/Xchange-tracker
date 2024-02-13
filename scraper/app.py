d0b4af55f76ae0681e745961888c6f2e0c71f759
Marcos Betancourt <mhernandez@cedar.com>
2024-02-12 20:54:11 -0400

    Added local connection to dynamodb

--- scraper/app.py
+++ scraper/app.py
@@ -1,2 +1,10 @@
+import boto3
+
+dynamodb = boto3.resource(
+    "dynamodb", endpoint_url="http://db:8000", region_name="us-east-1"
+)
+print(dynamodb)
+
+
 def handler(event, context):
-    print('Hello world')
+    print("Hello world")
