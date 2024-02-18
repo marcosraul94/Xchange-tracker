05a51ca4101f0dae8fa70d5d9a45a0f8b0664feb
Marcos Betancourt <mhernandez@cedar.com>
2024-02-18 13:11:10 -0400

    Updated dockerfile

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,6 @@
 import puppeteer from "puppeteer";
 
 export const handler = async () => {
-  console.log("here");
   const browser = await puppeteer.launch({
     headless: "new",
     timeout: 3000,
@@ -9,30 +8,14 @@ export const handler = async () => {
   });
   const page = await browser.newPage();
 
-  // Navigate the page to a URL
-  await page.goto("https://developer.chrome.com/");
-
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
-
-  // Type into search box
-  await page.type(".devsite-search-field", "automate beyond recorder");
-
-  // Wait and click on first result
-  const searchResultSelector = ".devsite-result-item-link";
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    "text/Customize and automate"
+  await page.goto("https://popularenlinea.com/personas/Paginas/Home.aspx");
+  const euroBuyElement = await page.waitForSelector(
+    "#compra_peso_euro_desktop"
   );
-  const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+  const value = await euroBuyElement.evaluate((el) => el.value);
+  console.log(`Buy euro rate ${value}`);
 
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
   await browser.close();
-  console.log("after");
 };
 
 handler();
